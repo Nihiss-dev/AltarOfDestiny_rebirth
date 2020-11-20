f566fe38fff65f2c0658481890edbfe23450d5d1
Alexis FELDMAR <alexisf@Backlight.local>
2020-11-20 20:00:58 +0100

    Create project

---
+++ Source/altarofdestiny/altarofdestinyGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "altarofdestinyGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AaltarofdestinyGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AaltarofdestinyGameMode();
+};
+
+
+
