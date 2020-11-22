58d6b430893bbde0ef8ee162415eddabcb1b196e
Alexis FELDMAR <alexisf@Backlight.local>
2020-11-22 15:18:54 +0100

    Add basic classes

---
+++ Source/altarofdestiny/Game/S_GameState.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameStateBase.h"
+#include "S_GameState.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ALTAROFDESTINY_API AS_GameState : public AGameStateBase
+{
+	GENERATED_BODY()
+	
+};
---
+++ Source/altarofdestiny/Utils/S_Types.cpp
@@ -0,0 +1,12 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "S_Types.h"
+
+S_Types::S_Types()
+{
+}
+
+S_Types::~S_Types()
+{
+}
