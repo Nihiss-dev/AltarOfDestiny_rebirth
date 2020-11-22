c4e93f71a62cba482dde6faeea8f865aa4b36f26
Alexis FELDMAR <alexisf@Backlight.local>
2020-11-22 13:28:13 +0100

    clean, create right classes, and add replication of player

---
+++ Source/altarofdestiny/S_GameMode.cpp
@@ -0,0 +1,9 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "S_GameMode.h"
+#include "S_Character.h"
+#include "UObject/ConstructorHelpers.h"
+
+AS_GameMode::AS_GameMode()
+{
+}
---
+++ Source/altarofdestiny/S_GameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "S_GameMode.generated.h"
+
+UCLASS(minimalapi)
+class AS_GameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AS_GameMode();
+};
+
+
+
